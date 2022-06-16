7a91bd197747cf5a0edce9872278a5b5a2fa124b
Darkoore <80989378+Darkoore@users.noreply.github.com>
2022-06-15 20:33:24 -0400

    Se añade ViewModel y se genera ParticipantesList

---
+++ Models/ParticipantesList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Compromisos.Models
+{
+    public class ParticipantesList
+    {
+        [Display(Name = "PART_RUT")]
+        public string PART_RUT { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        public string PART_NOMBRE { get; set; }
+        [Required(ErrorMessage = "Fono is required.")]
+        public string PART_FONO { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        public string PART_EMAIL { get; set; }
+        [Required(ErrorMessage = "Institucion is required.")]
+        public string PART_INSTITUCION { get; set; }
+    }
+}
---
+++ Models/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Compromisos.Models
+{
+    public class ViewModel
+    {
+        public ICollection <ParticipantesList> ParticipantesLists { get; set; }
+        public Participantes Participantes { get; set; }
+    }
+}
